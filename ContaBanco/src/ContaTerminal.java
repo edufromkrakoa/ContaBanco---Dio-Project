39457268154ddb15fa66ec41a20c0f93b59bdd97
edufromkrakoa <wikialmeida@gmail.com>
2024-07-13 17:13:17 -0300

    meu projeto conta banco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,41 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        //Definindo variaveis
+        String nomeCliente;
+        String sobrenomeCliente;
+        String agencia;
+        double saldo;
+        int numero;
+
+        //TODO: Conhecer e importar a classe scanner
+        Scanner scanner = new Scanner(System.in);
+        
+        // exibir as mensagens para o usuario
+        System.out.println("Bem vindo, vamos criar uma conta em nosso banco!"); 
+        System.out.println("\nPor favor nos informe seu nome e sobrenome: ");  
+        nomeCliente = scanner.next();
+        sobrenomeCliente = scanner.next();
+
+        System.out.println("Nos informe o numero da sua agencia: ");
+        agencia = scanner.next();
+
+        System.out.println("Nos informe o numero da sua conta: ");
+        numero = scanner.nextInt();
+
+        System.out.println("Nos informe o saldo inicial da conta: ");
+        saldo = scanner.nextDouble();
+
+        // Obter pela scanner os valores digitados no terminal 
+
+        // Exibir a mensagem da conta criada
+        System.out.println("Bem-vindo, " + nomeCliente + " " + sobrenomeCliente + "! Sua conta foi criada com sucesso!");
+        System.out.println("Aqui está os dados da sua conta: ");
+        System.out.println("Agência: " + agencia);
+        System.out.println("Número da Conta: " + numero);
+        System.out.println("Saldo: " + saldo);
+     
+        scanner.close();
     }
 }
